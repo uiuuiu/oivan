23e981ae0d9b9dba8057c41b32480ede31770d34
dypd7209 <dypd7209@co-well.com.vn>
2021-06-10 17:00:50 +0700

    cahnge user name of server

---
+++ setup/prd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# deploy.sh
+
+# Put this script on your production server and make it executable with chmod
+# +x. 
+
+# Set the deploy_dir variable to the path on the production server to
+# deployment directory. The script assumes the deployment directory is a git
+# clone of the codebase from an upstream git repo. This script also assumes
+# you are using Passenger.
+
+deploy_dir=~/srv/oivan/prd # deploy dir
+
+# You then can run deploy.sh  remotely with ssh:
+#
+#     ssh user@deployserver "~/deploy.sh"
+
+
+echo "=======> Setup to $deploy_dir <========"
+cd $deploy_dir && bundle install --path=vendor/bundle
+start_prd
